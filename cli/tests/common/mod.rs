3553be36e7834e023867bf95fb9471a5c1e0d7e1
XX <freecoder.xx@gmail.com>
2025-02-17 20:39:41 +0300

    refactor: move run test cases code to the common mod

--- cli/tests/common/mod.rs
+++ cli/tests/common/mod.rs
@@ -1,6 +1,8 @@
 use std::io;
 use std::path::Path;
 
+use temp_testdir::TempDir;
+
 use self::case::TestSection;
 
 pub mod case;
@@ -10,4 +12,27 @@ pub const CARGO_BIN_ALIAS: &str = "todo";
 
 pub fn parse_tests_from_markdown(md_file_path: impl AsRef<Path>) -> io::Result<Vec<TestSection>> {
     case::parse_tests_from_markdown(md_file_path, Some(CARGO_BIN_ALIAS))
+}
+
+pub fn run_test_cases(md_file_path: impl AsRef<Path>) -> anyhow::Result<()> {
+    let sections = parse_tests_from_markdown(md_file_path)?;
+
+    for section in sections {
+        let test_dir = TempDir::default();
+        let mut completed_tests = Vec::new();
+
+        println!("\n# {}", section.title);
+
+        for test_case in section.cases {
+            let test_case = test_case.with_test_dir(&test_dir.as_os_str());
+
+            println!("Testing: {:?}", test_case.commands);
+            test_case.run()?;
+            completed_tests.push(test_case);
+        }
+
+        // Destroy completed test cases
+        drop(completed_tests);
+    }
+    Ok(())
 }
--- cli/tests/new_project.rs
+++ cli/tests/new_project.rs
@@ -1,28 +1,8 @@
-use temp_testdir::TempDir;
-
-use self::common::parse_tests_from_markdown;
+use self::common::run_test_cases;
 
 mod common;
 
 #[test]
 fn new_project_test_cases() {
-    let sections = parse_tests_from_markdown("tests/new_project.md").unwrap();
-
-    for section in sections {
-        let test_dir = TempDir::default();
-        let mut completed_tests = Vec::new();
-
-        println!("\n# {}", section.title);
-
-        for test_case in section.cases {
-            let test_case = test_case.with_test_dir(&test_dir.as_os_str());
-
-            println!("Testing: {:?}", test_case.commands);
-            test_case.run().unwrap();
-            completed_tests.push(test_case);
-        }
-
-        // Destroy completed test cases
-        drop(completed_tests);
-    }
+    run_test_cases("tests/new_project.md").unwrap();
 }
