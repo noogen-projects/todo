313ec43bcc287bd44a08cb8e684ac5f00ec6fe99
XX <freecoder.xx@gmail.com>
2025-07-05 12:29:42 +0300

    feat: extract tester to md-cli-test crate

--- cli/tests/common/mod.rs
+++ cli/tests/common/mod.rs
@@ -1,38 +1,12 @@
-use std::io;
-use std::path::Path;
+use std::path::PathBuf;
 
-use temp_testdir::TempDir;
+use md_cli_test::Tester;
+use todo_app::config::{DEFAULT_CONFIG_FILE_NAME, ROOT_CONFIG_ENV_KEY};
 
-use self::case::TestSection;
+pub fn run_test_cases(md_file_path: impl Into<PathBuf>) -> anyhow::Result<()> {
+    Tester::new(md_file_path)
+        .with_env(ROOT_CONFIG_ENV_KEY, format!("./{DEFAULT_CONFIG_FILE_NAME}"))
+        .run()?;
 
-pub mod case;
-pub mod cmd;
-
-pub const CARGO_BIN_ALIAS: &str = "todo";
-
-pub fn parse_tests_from_markdown(md_file_path: impl AsRef<Path>) -> io::Result<Vec<TestSection>> {
-    case::parse_tests_from_markdown(md_file_path, Some(CARGO_BIN_ALIAS))
-}
-
-pub fn run_test_cases(md_file_path: impl AsRef<Path>) -> anyhow::Result<()> {
-    let sections = parse_tests_from_markdown(md_file_path)?;
-
-    for section in sections {
-        let test_dir = TempDir::default();
-        let mut completed_tests = Vec::new();
-
-        println!("\n# {}", section.title);
-
-        for test_case in section.cases {
-            let test_case = test_case.with_test_dir(&test_dir.as_os_str());
-
-            println!("Testing: {:?}", test_case.commands);
-            test_case.run()?;
-            completed_tests.push(test_case);
-        }
-
-        // Destroy completed test cases
-        drop(completed_tests);
-    }
     Ok(())
 }
